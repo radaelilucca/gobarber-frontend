59b4d59163ecba3b97be5a11ed0b14869a9f2284
Lucca <radaelidev@gmail.com>
2020-05-23 20:33:18 -0300

    Reactotron setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Router} from 'react-router-dom'
 import Routes from './routes'
 import history from './services/history'
 
+import './config/ReactotronConfig'
 
 
 function App() {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
-import Dashboard from '../pages/Dashboard'
-import Profile from '../pages/Profile'
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 
 export default function Routes(){
@@ -14,6 +14,7 @@ export default function Routes(){
     <Switch>
       <Route path="/" exact component={SignIn}/>
       <Route path="/register" component={SignUp}/>
+
       <Route path="/dashboard" component={Dashboard}/>
       <Route path="/profile" component={Profile}/>
     </Switch>
