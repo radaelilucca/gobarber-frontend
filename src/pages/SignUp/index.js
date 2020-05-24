25ef5b58b4aee410f92e4f3108b957d83a25ccb1
Lucca <radaelidev@gmail.com>
2020-05-24 19:03:16 -0300

    Added Unform to handle forms

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,23 +1,31 @@
 import React from "react";
+import { Form, Input } from "@rocketseat/unform";
 
 import { Link } from "react-router-dom";
 
 import logo from "~/assets/logo.svg";
 
 function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <form>
-        <input placeholder="Seu e-mail" />
-        <input placeholder="Nome Completo" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" placeholder="Seu e-mail" />
+        <Input name="name" placeholder="Nome Completo" />
 
-        <input type="password" placeholder="Sua senha secreta" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
         <button type="submit">Criar Conta</button>
 
         <Link to="/">Já tenho uma conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
